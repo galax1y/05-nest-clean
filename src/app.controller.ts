afaf7b41fc92949df47c64c38eb50a0484e6043b
LucasWermann <lucasvw.eng@gmail.com>
2023-09-07 16:49:36 -0300

    Add: Custom Eslint config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
 
+// Decorador @Controller e decoradores de rotas podem receber strings para customizar o nome de acesso das rotas.
 @Controller()
 export class AppController {
+  // Inversão de dependências
   constructor(private readonly appService: AppService) {}
 
+  // Decorador de rota @Get
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
   }
 }
