0e7b441dfb92f9c5950516af10e8d800365e6876
LucasWermann <lucasvw.eng@gmail.com>
2023-09-10 16:30:52 -0300

    Add: ConfigModule no NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
+import { envSchema } from './env'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 
 import { CreateAccountController } from './controllers/create-account.controller'
-
 import { PrismaService } from './prisma/prisma.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import { Env } from './env'
+
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3333)
+
+  const configService: ConfigService<Env> = app.get(ConfigService)
+  const port = configService.get('PORT')
+
+  await app.listen(port)
 }
 
 bootstrap()
