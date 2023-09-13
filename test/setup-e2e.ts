cf062eaf41a404a02693470de009dbad5b308beb
LucasWermann <lucasvw.eng@gmail.com>
2023-09-13 14:39:47 -0300

    Add: New database for every e2e test configuration

---
+++ test/setup-e2e.ts
@@ -0,0 +1,39 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+
+import { beforeAll, afterAll } from 'vitest'
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Missing DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+
+const schemaUUID = randomUUID()
+
+// Generate a schema for every e2e test
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseURL(schemaUUID)
+
+  process.env.DATABASE_URL = databaseURL
+
+  execSync('npx prisma migrate deploy')
+})
+
+// Clear it after the test is done
+// prettier-ignore
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaUUID}" CASCADE`)
+  await prisma.$disconnect()
+})
