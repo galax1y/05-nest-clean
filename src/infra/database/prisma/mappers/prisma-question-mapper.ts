fbbd301902b43e0516504c2d97f8d0a2f84c7d20
galax1y <lucasvw.eng@gmail.com>
2023-09-15 12:12:01 -0300

    Add: Mapper, Prisma Question > Domain Question

---
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -0,0 +1,23 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { Question as PrismaQuestion } from '@prisma/client'
+
+// A Mapper is a class responsible for the conversion of data structures between the layers of the application
+
+export class PrismaQuestionMapper {
+  static toDomain(raw: PrismaQuestion): Question {
+    return Question.create(
+      {
+        title: raw.title,
+        content: raw.content,
+        authorId: new UniqueEntityID(raw.authorId),
+        bestAnswerId: undefined,
+        slug: Slug.create(raw.slug),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+}
--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -4,15 +4,27 @@ import { PaginationParams } from '@/core/repositories/pagination-params'
 
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { PrismaService } from '../prisma.service'
+import { PrismaQuestionMapper } from '../mappers/prisma-question-mapper'
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
+  constructor(private prisma: PrismaService) {}
+
   create(question: Question): Promise<void> {
     throw new Error('Method not implemented.')
   }
 
-  findById(questionId: string): Promise<Question | null> {
-    throw new Error('Method not implemented.')
+  async findById(questionId: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        id: questionId,
+      },
+    })
+
+    if (!question) return null
+
+    return PrismaQuestionMapper.toDomain(question)
   }
 
   delete(question: Question): Promise<void> {
