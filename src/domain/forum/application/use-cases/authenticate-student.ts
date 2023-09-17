2a152ea3d959875c2942637f4f32ee4be3f58d45
galax1y <lucasvw.eng@gmail.com>
2023-09-17 15:02:09 -0300

    Add: Student register and authenticate use cases

---
+++ src/domain/forum/application/use-cases/authenticate-student.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common'
+import { Either, left, right } from '@/core/either'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+import { Encrypter } from '../cryptography/encrypter'
+import { HashComparer } from '../cryptography/hash-comparer'
+
+import { StudentsRepository } from '../repositories/students-repository'
+
+interface AuthenticateStudentUseCaseRequest {
+  email: string
+  password: string
+}
+
+type AuthenticateStudentUseCaseResponse = Either<
+  InvalidCredentialsError,
+  { accessToken: string }
+>
+
+@Injectable()
+export class AuthenticateStudentUseCase {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private hashComparer: HashComparer,
+    private encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateStudentUseCaseRequest): Promise<AuthenticateStudentUseCaseResponse> {
+    const student = await this.studentsRepository.findByEmail(email)
+
+    if (!student) {
+      return left(new InvalidCredentialsError())
+    }
+
+    const passwordMatches = await this.hashComparer.compare(
+      password,
+      student.password,
+    )
+
+    if (!passwordMatches) {
+      return left(new InvalidCredentialsError())
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: student.id.toString(),
+    })
+
+    return right({ accessToken })
+  }
+}
