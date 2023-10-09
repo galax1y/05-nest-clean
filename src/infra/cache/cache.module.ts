5c21b8223066d2cd94c87545a414192ea02d7b55
galax1y <lucasvw.eng@gmail.com>
2023-10-09 17:32:35 -0300

    Add: Cache implementation w/ Redis

--- src/infra/cache/cache.module.ts
+++ src/infra/cache/cache.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common'
+import { CacheRepository } from './cache-repository'
+import { RedisCacheRepository } from './redis/redis-cache-repository'
+import { RedisService } from './redis/redis.service'
 import { EnvModule } from '../env/env.module'
-import { EnvService } from '../env/env.service'
 
 @Module({
   imports: [EnvModule],
-  providers: [EnvService],
+  providers: [
+    RedisService,
+    { provide: CacheRepository, useClass: RedisCacheRepository },
+  ],
+  exports: [CacheRepository],
 })
 export class CacheModule {}
